cc568c8d4b80753e8b7a64261e0822d3be18a122
JOLODOVSKY URIEL MATIAS <46960341@ort>
2023-04-17 10:32:57 -0300

    First 35 minutes finished

--- next-test/pages/api/index.ts
+++ next-test/pages/api/index.ts
@@ -4,6 +4,14 @@ const prisma = new PrismaClient();
 async function main() {
 // Here we write the queries
 await prisma.user.deleteMany()
+const user = await prisma.user.create({
+    data: {
+        name: "Uriel",
+        email: "urieljolo@gmail.com",
+        age: 17
+    }
+})
+console.log(user);
 } 
 
 main()
